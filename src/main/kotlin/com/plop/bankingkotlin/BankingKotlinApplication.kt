cb4b7863186d3c90120b62b534ab2bec54153eca
Gaël Laurent <gael.laurent@octo.com>
2022-01-14 18:46:10 +0100

    init: init the project with kotlin, gradle and spring boot

---
+++ src/main/kotlin/com/plop/bankingkotlin/BankingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.plop.bankingkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankingKotlinApplication>(*args)
+}
