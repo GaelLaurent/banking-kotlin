3ec182ec1e3db4d2ad78ea1dc6f36e958556b6e6
Gaël Laurent <gael.laurent@octo.com>
2022-01-31 13:50:32 +0100

    refactor(kotlin): make the constructor a primary one in th EventDispatcher

--- src/main/kotlin/com/plop/bankingkotlin/buildingBlocks/EventDispatcher.kt
+++ src/main/kotlin/com/plop/bankingkotlin/buildingBlocks/EventDispatcher.kt
@@ -2,14 +2,12 @@ package com.plop.bankingkotlin.buildingBlocks
 
 import kotlin.reflect.KClass
 
-class EventDispatcher: EventBusMiddleware {
+class EventDispatcher(eventHandlers: List<EventHandler<out DomainEvent>>) : EventBusMiddleware {
 
     private var registeredCommandHandlers: Map<KClass<out DomainEvent>, List<EventHandler<out DomainEvent>>> = mapOf()
 
-    constructor(eventHandlers: List<EventHandler<out DomainEvent>>) {
-
+    init {
         registeredCommandHandlers = eventHandlers.groupBy { it.isAssignedTo() }
-
     }
 
     override fun <E : DomainEvent> dispatch(event: E) {
